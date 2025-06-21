56f8ce21967d0f158dbbd5c9131f214f961de393
Chamal1120 <kenway213beep@gmail.com>
2025-06-21 17:41:52 +0530

    add sigle char handling to lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,6 @@
 use crate::token::{Token, TokenType};
 
+
 pub struct Lexer {
     source: String,
     chars: Vec<char>,
@@ -25,7 +26,7 @@ impl Lexer {
 
         while !self.is_at_end() {
             self.start = self.current;
-            if let Some(Token) = self.scan_token() {
+            if let Some(token) = self.scan_token() {
                 tokens.push(token);
             }
         }
@@ -33,14 +34,57 @@ impl Lexer {
         tokens.push(Token {
             token_type: TokenType::Eof,
             lexeme: "".to_string(),
-            line.self.line,
+            line: self.line,
         });
 
         tokens
     }
 
     fn is_at_end(&self) -> bool {
+        self.current >= self.chars.len()
+    }
+
+    fn advance(&mut self) -> char {
         let ch = self.chars[self.current];
         self.current += 1;
+        ch
+    }
+
+    // For now: placeholder. logic is yet to be added
+    fn scan_token(&mut self) -> Option<Token> {
+        let ch = self.advance();
+
+        match ch {
+            '(' => Some(self.make_token(TokenType::LeftParen)),
+            ')' => Some(self.make_token(TokenType::RightParen)),
+            '{' => Some(self.make_token(TokenType::LeftBrace)),
+            '}' => Some(self.make_token(TokenType::RightBrace)),
+            ',' => Some(self.make_token(TokenType::Comma)),
+            '.' => Some(self.make_token(TokenType::Dot)),
+            '-' => Some(self.make_token(TokenType::Minus)),
+            '+' => Some(self.make_token(TokenType::Plus)),
+            ';' => Some(self.make_token(TokenType::SemiColon)),
+            '*' => Some(self.make_token(TokenType::Star)),
+
+            ' ' | '\r' | '\t' => None,
+            '\n' => {
+                self.line += 1;
+                None
+            }
+            _ => {
+                // Later: handle indentifiers, numbers, keywords
+                println!("Unexpected charcter: {}", ch);
+                None
+            }
+        }
+    }
+
+    fn make_token(&self, token_type: TokenType) -> Token {
+        let text: String = self.chars[self.start..self.current].iter().collect();
+        Token {
+            token_type,
+            lexeme: text,
+            line: self.line,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,24 @@ mod linter;
 mod error;
 
 use token::{Token, TokenType};
+use lexer::Lexer;
 
 fn main() {
     println!("Welcome to BlazeLint! ");
 
-    let example_token = Token {
-        token_type: TokenType::Var,
-        lexeme: "Var".to_string(),
-        line: 1,
-    };
+    let source = "() { } , . - + * ;".to_string();
+    let mut lexer = Lexer::new(source);
+    let tokens = lexer.scan_tokens();
 
-    println!("{:?}", example_token);
+    for token in tokens {
+        println!("{:?}", token);
+    }
+
+//    let example_token = Token {
+//        token_type: TokenType::Var,
+//        lexeme: "Var".to_string(),
+//        line: 1,
+//    };
+//
+//    println!("{:?}", example_token);
 }
